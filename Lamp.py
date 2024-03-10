d0fb1fe1e74ba527ba08c5e87442ceac44b94119
Wallychopes <162908783+Wallychopes@users.noreply.github.com>
2024-03-10 18:35:04 +0000

    i like trains

--- Lamp.py
+++ Lamp.py
@@ -1,3 +1,12 @@
-print("1+2")
-print("=")
-print(3)
+def squares_numbers(5674)
+"""Return a list of the squares of the numbers."""
+squares = [] 
+for numbers in numbers:
+    squares.append(number**2)
+return squares
+
+
+def main():
+    numbers = [1, 2, 3, 4]
+    result = square_numbers(numbers)
+    print(result)
